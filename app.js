dbdbc9588f03ec683370cadc9a0fe2e57435a8dc
Alan Dean <adean3032@gmail.com>
2022-11-14 03:47:47 -0500

    Added the interval, added dead snake color,  added checkHits function, changed classList adjustments to be add/remove instead of toggle, fixed high score display, added restart function for tryAgain button, and disallowed the snake direction to change to run into itself

--- app.js
+++ app.js
@@ -1,73 +1,108 @@
-// // state
-// let state;
-
-// function buildInitialState() {
-
-// }
-
-// // render
-// function renderState() {
-
-// }
-
-// // maybe a dozen or so helper functions for tiny pieces of the interface
-
-// // listeners
-// function onBoardClick() {
-//   // update state, maybe with another dozen or so helper functions...
-
-//   renderState() // show the user the new state
-// }
-// const board = document.getElementById('board');
-// board.addEventListener('click', onBoardClick); // etc
-
-// // add to above
-// function tick() {
-//     // this is an incremental change that happens to the state every time you update...
-  
-//     renderState()
-//   }
-  
-//   setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
-  
-//   // now you might have things like
-//   document.addEventListener('keydown', function (event) {
-//     // here you might read which key was pressed and update the state accordingly
-//   })
-
-//   Code that I've made without tutorial guidance^^
 let gameBoard = document.getElementById('board');
 let startButton = document.getElementById('start_button');
 let title = document.getElementById('game_title');
 let tryAgain = document.getElementById('try_again_button');
 let currentScoreDisplay = document.getElementById('score');
-let currentScore = currentScoreDisplay.value;
+let currentScore 
 let highScoreDisplay = document.getElementById('high_score');
-let highScore = highScoreDisplay.value;
+let highScore 
 let gameRow = document.getElementsByClassName('game_row');
 let gameSpace = document.getElementsByClassName('game_space');
-let direction = 1;
+let direction = 0;
 let snake = [143, 142, 141, 140];
+let intervalTime = 0;
+let interval = 0;
+let newSpace;
 
-function makeSnake () {
+function makeSnake (life) {
   for (let i = 0; i < snake.length; ++i) {
     let snakeSpace = snake[i];
-    gameSpace[snakeSpace].classList.add('snake');
+    if (life) {
+      gameSpace[snakeSpace].classList.add('snake'); 
+    } else {
+      gameSpace[snakeSpace].classList.add('dead_snake'); 
+    }
   }
 }
 
+function moveOnce () {
+  let tail = snake[snake.length-1];
+  let tailSpace = gameSpace[tail];
+  tailSpace.classList.remove('snake');
+  snake.pop();
+
+  newSpace = snake[0] + direction;
+  gameSpace[newSpace].classList.add('snake');
+  snake.unshift(newSpace);
+  eatApple(tail);
+}
+
+function slither () {
+  if (checkHits()) {
+    makeSnake(false);
+    alert('You hit something');
+    tryAgain.classList.remove('nodisplay');
+    return clearInterval(interval);
+  } else {
+    moveOnce();
+  }
+}
+
+function eatApple (poppedTail) {
+  if (gameSpace[snake[0]].classList.contains('apple')) {
+    gameSpace[snake[0]].classList.remove('apple');
+    snake.push(poppedTail);
+    gameSpace[poppedTail].classList.add('snake');
+    newApple();
+    currentScore++;
+    currentScoreDisplay.innerText = `Score: ${currentScore}`;
+    if (highScore === currentScore - 1) {
+      highScore = currentScore;
+      highScoreDisplay.innerText = `High Score: ${highScore}`;
+    }
+  }
+}
+
+function checkHits () {
+  if (
+    // If snake hit top 
+    (snake[0] - 17 < 0 && direction === -17) ||
+    // or snake hit bottom
+    (snake[0] + 17 > 288 && direction === 17) ||
+    // or snake hit left 
+    (snake[0] % 17 === 0 && direction === -1) ||    
+    // or snake hit right
+    (snake[0] % 17 === 16 && direction === 1) ||
+    // or snake hit itself 
+    (gameSpace[snake[0] + direction].classList.contains('snake'))
+  ) {
+    // snake hit something
+    return true;
+  } else {
+    // snake hit nothing
+    return false
+  }
+}
 
 function startingState () {
-  title.classList.toggle('post_start_title');
+  title.classList.add('post_start_title');
   startButton.classList.add('nodisplay');
   gameBoard.classList.remove('nodisplay');
   gameBoard.classList.add('yesdisplay');
   currentScore = 0;
-    if (highScore === undefined) {
-      highScore = currentScore;
-    }
+  direction = 1;
+  intervalTime = 250;
+  snake = [143, 142, 141, 140];
+  interval = setInterval(slither, intervalTime);
   currentScoreDisplay.classList.remove('nodisplay');
+  currentScoreDisplay.innerText = `Score: ${currentScore}`;
   highScoreDisplay.classList.remove('nodisplay');
+  if (highScore === undefined) {
+    highScore = currentScore;
+    highScoreDisplay.innerText = `High Score: ${currentScore}`;
+  } else {
+    highScoreDisplay.innerText = `High Score: ${highScore}`;
+  }
     let div = document.createElement('div');
     gameBoard.appendChild(div);
     div.classList.add('game_row');
@@ -76,25 +111,21 @@ function startingState () {
       div.appendChild(divChild); 
       divChild.classList.add('game_space');
     }
-  makeSnake ();
-  newApple ();
+    newApple();
+    makeSnake(true);
   }
 
 function newApple () {
   function appleSpaceFinder () {
-    let appleSpaceLocal = Math.floor(Math.random() * gameRow.length);
+    let appleSpaceLocal = Math.floor(Math.random() * 289);
     return appleSpaceLocal;
   }
+
   for (let i = 0; i !== -1; ++i) {
-    let appleElem = document.getElementsByClassName('apple');
-    console.log(appleElem);
     let appleSpace = appleSpaceFinder();
-    if (!gameRow[appleSpace].classList.contains('snake')) {
-      let newApple = gameRow[appleSpace];
-      console.log(newApple);
-      console.log(gameRow);
+    if (!gameSpace[appleSpace].classList.contains('snake')) {
+      let newApple = gameSpace[appleSpace];
       newApple.classList.add('apple');
-      console.log('Apple added!');
       return;
     } else {
       continue;
@@ -102,27 +133,42 @@ function newApple () {
  }
 }
 
+function restart () {
+  for (let i = 0; i < 289; ++i) {
+    let currentSpace = gameSpace[i];
+    if (currentSpace.classList.contains('snake')) {
+      currentSpace.classList.remove('snake');
+    }
+    if (currentSpace.classList.contains('dead_snake')) {
+      currentSpace.classList.remove('dead_snake');
+    }
+    if (currentSpace.classList.contains('apple')) {
+      currentSpace.classList.remove('apple');
+    }
+  }
+  currentScore = 0;
+  direction = 1;
+  intervalTime = 250;
+  snake = [143, 142, 141, 140];
+  interval = setInterval(slither, intervalTime);
+  tryAgain.classList.add('nodisplay');
+  newApple();
+  makeSnake(true);
+}
+
 startButton.addEventListener('click', startingState);
-// On keydowns, change the direction
+tryAgain.addEventListener('click', restart);
 document.addEventListener('keydown', function (event) {
-  // here you might read which key was pressed and update the state accordingly
-  if(event.code === 'ArrowLeft' || event.code === 'KeyA') {
+  if((event.code === 'ArrowLeft' || event.code === 'KeyA') && direction !== 1) {
     direction = -1;
   };
-  if(event.code === 'ArrowUp' || event.code === 'KeyW') {
+  if((event.code === 'ArrowUp' || event.code === 'KeyW') && direction !== 17) {
     direction = -17;
   };
-  if(event.code === 'ArrowRight' || event.code === 'KeyD') {
+  if((event.code === 'ArrowRight' || event.code === 'KeyD') && direction !== -1) {
     direction = 1;
   };
-  if(event.code === 'ArrowDown' || event.code === 'KeyS') {
+  if((event.code === 'ArrowDown' || event.code === 'KeyS') && direction !== -17) {
     direction = 17;
   };
 });
-setInterval (function gameplayState () {
-  currentScoreDisplay.innerText = `Score: ${currentScore}`
-  highScoreDisplay.innerText = `High Score: ${highScore}`;
-  // Log previous highScore here instead;
-
-}, 
-300);
